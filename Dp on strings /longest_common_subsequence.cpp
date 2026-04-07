a6d6fecb21c63eccce2bcb4f6f68d803509af430
Abhi Singh <abhisinghind90@gmail.com>
2026-04-07 23:04:59 +0530

    improved with dp

--- Dp on strings /longest_common_subsequence.cpp
+++ Dp on strings /longest_common_subsequence.cpp
@@ -1,27 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int original(int ind1, int ind2, string &s1, string &s2) {
+int original(int ind1, int ind2, string &s1, string &s2,
+             vector<vector<int>> &dp) {
   if (ind1 < 0 || ind2 < 0)
     return 0;
 
+  if (dp[ind1][ind2] != -1)
+    return dp[ind1][ind2];
+
   if (s1[ind1] == s2[ind2])
-    return 1 + original(ind1 - 1, ind2 - 1, s1, s2);
+    return dp[ind1][ind2] = 1 + original(ind1 - 1, ind2 - 1, s1, s2, dp);
   else
-    return 0 + max(original(ind1 - 1, ind2, s1, s2),
-                   original(ind1, ind2 - 1, s1, s2));
+    return dp[ind1][ind2] = 0 + max(original(ind1 - 1, ind2, s1, s2, dp),
+                                    original(ind1, ind2 - 1, s1, s2, dp));
 }
 
 int helper(string &s1, string &s2) {
   int n = s1.size();
   int m = s2.size();
 
-  return original(n - 1, m - 1, s1, s2);
+  vector<vector<int>> dp(n, vector<int>(m, -1));
+
+  return original(n - 1, m - 1, s1, s2, dp);
 }
 
 int main() {
   string s1 = "abcd";
-  string s2 = "abrt";
+  string s2 = "abzc";
 
   int ans = helper(s1, s2);
 
