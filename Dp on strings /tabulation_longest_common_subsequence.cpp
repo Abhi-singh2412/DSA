6b7f756e10594c46f5877f65f2a1a44db0bd2ba1
Abhi Singh <abhisinghind90@gmail.com>
2026-04-07 23:19:01 +0530

    tabulation commit

---
+++ Dp on strings /tabulation_longest_common_subsequence.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int helper(string &s1, string &s2) {
+  int n = s1.size();
+  int m = s2.size();
+  vector<vector<int>> dp(
+      n + 1,
+      vector<int>(
+          m + 1,
+          -1)); // i did n + 1 and m +1 because this dp matrix is 1 based
+                // indexing not 0 based look at striver video u will understand
+                // (it is mainly because u cant represent -1 contion in the base
+                // case as u did in memoization so here index 0 is treated as
+                // index -1 and similarily index 2 will be index 1 and index 4
+                // will be index 3 so onn)
+
+  // base case
+  for (int j = 0; j <= m; j++)
+    dp[0][j] = 0;
+  for (int i = 0; i <= n; i++)
+    dp[i][0] = 0; // here [i][0] represents [i][-1] and similariy same for j
+
+  // tabulation
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      if (s1[i - 1] == s2[j - 1])
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      else
+        dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
+    }
+  }
+  return dp[n][m];
+}
+
+int main() {
+  string s1 = "csyuhakcaukc";
+  string s2 = "cahuwkauchucxahu";
+
+  int ans = helper(s1, s2);
+  cout << ans << " ";
+}
